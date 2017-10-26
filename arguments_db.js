04f21a84ac0dc332d389f02ec4ac93bc4ae039bb
Patrick Bowen <phunanon@gmail.com>
2017-10-26 04:23:42 +0100

    Seperate some JS and HTML

---
+++ arguments_db.js
@@ -0,0 +1,31 @@
+var arguments_DB = [
+
+    ["Markets foster innovation", "innovation, entrepreneurship, markets", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+
+    ["Stalin manufactured famine in The Ukraine", "famine, The Ukraine, Soviet, Union, Stalin, 1932", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+
+    ["Communism is against human nature", "human, nature, unnatural", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+
+    ["Markets result in perfect allocation of resources", "markets, allocation, perfect, resources", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+
+    ["Communism is undemocratic", "democracy, dictatorship, undemocratic", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+
+    ["Communism has never worked", "work, success, failure, achieve", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+
+    ["Communism killed millions of people", "killed, deaths, millions, died", "", "Iraq/Middle East"],
+
+    ["Communism is too corruptible", "corruption", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+
+    ["Venezuela is socialist", "Venezuela", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+
+    ["The Soviet Union was allied with Nazi Germany", "Nazism, Germany, Soviet, Union", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+
+    ["Socialist countries are always poor and hungry", "poverty, poor, hungry, hunger, famine, food, bread", "", "Burkina Faso"],
+
+    ["Communist countries are anti-LGBT/civil rights", "LGBT, gay, rights, civil", "", "Burkina Faso"]
+
+];
+
+/* SOURCES FOR RESEARCH:
+https://www.reddit.com/r/communism/comments/6uhar8/comrades_reading_the_declassified_post_ww2/
+*/
