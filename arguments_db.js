000ccdbf185affa883aa271f1e5606d4820a4629
phunanon <phunanon@gmail.com>
2017-11-02 15:54:54 +0000

    Added Burkina Faso information; added guide & rules

--- arguments_db.js
+++ arguments_db.js
@@ -1,34 +1,78 @@
 var arguments_DB = [
 
-    ["Markets foster innovation", "innovation, entrepreneurship, markets", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+    ["Markets foster innovation",
+    "innovation, entrepreneurship, markets",
+    "Short rebuttals",
+    "Long, sourced counter-arguments"],
 
-    ["Stalin manufactured famine in The Ukraine", "famine, The Ukraine, Soviet, Union, Stalin, 1932", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+    ["Stalin manufactured famine in The Ukraine",
+    "famine, The Ukraine, Soviet, Union, Stalin, 1932",
+    "Short rebuttals",
+    "Long, sourced counter-arguments"],
 
-    ["Communism is against human nature", "human, nature, unnatural, greed, greedy", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+    ["Communism is against human nature",
+    "human, nature, unnatural, greed, greedy",
+    "Short rebuttals",
+    "Long, sourced counter-arguments"],
 
-    ["Markets result in perfect allocation of resources", "markets, allocation, perfect, resources", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+    ["Markets result in perfect allocation of resources",
+    "markets, allocation, perfect, resources",
+    "Short rebuttals",
+    "Long, sourced counter-arguments"],
 
-    ["Communism is undemocratic", "democracy, dictatorship, undemocratic", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+    ["Communism is undemocratic",
+    "democracy, dictatorship, undemocratic",
+    "Short rebuttals",
+    "Long, sourced counter-arguments"],
 
-    ["Communism has never worked", "work, success, failure, achieve", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+    ["Communism has never worked",
+    "work, success, failure, achieve",
+    "Short rebuttals",
+    "Long, sourced counter-arguments"],
 
-    ["Communism killed millions of people", "killed, deaths, millions, died", "", "Iraq/Middle East"],
+    ["Communism killed millions of people",
+    "killed, deaths, millions, died",
+    "Short rebuttals",
+    "Iraq/Middle East"],
 
-    ["Communism is too corruptible", "corruption", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+    ["Communism is too corruptible",
+    "corruption",
+    "Short rebuttals",
+    "Long, sourced counter-arguments"],
 
-    ["Venezuela is socialist", "Venezuela", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+    ["Venezuela is socialist",
+    "Venezuela",
+    "Short rebuttals",
+    "Long, sourced counter-arguments"],
 
-    ["The Soviet Union was allied with Nazi Germany", "Nazism, Germany, Soviet, Union", "Short theoretical rebuttal", "Long, sourced counter-argument"],
+    ["The Soviet Union was allied with Nazi Germany",
+    "Nazism, Germany, Soviet, Union",
+    "Short rebuttals",
+    "Long, sourced counter-arguments"],
 
-    ["Socialist countries are always poor and hungry", "poverty, poor, hungry, hunger, famine, food, bread", "", "Burkina Faso"],
+    ["Socialist countries are always poor and hungry",
+    "poverty, poor, hungry, hunger, famine, food, bread",
+    "<li>Within four years (1983-1987) Burkina Faso reached food self-sufficiency, producing double the regional average for wheat production. Before this, the old state ('Upper Volta') was a food-importing French neo-colonial capitalist state.[https://web.archive.org/web/20170929035718/http://africanagenda.net/africas-che-guevara-and-burkina-faso/]</li>",
+    "Long, sourced counter-arguments"],
 
-    ["Communist countries are anti-LGBT/civil rights", "LGBT, gay, rights, civil, homosexual", "While it is unfortunately true that countries such as the USSR had mixed policy on homosexuality, other movements for their time were progressive in decriminalising such acts, and to this day most Communist movements are pro-LGBT. Russia in the USSR was also <b>the first</b> major world power to <a href=\"https://books.google.com/books?id=Qk-ICsx5L90C\">grant women suffrage</a>.<br>Burkina Faso under the leadership of Marxist revolutionary Thomas Sankara banned female genital mutilation, forced marriages and polygamy, while appointing women to high governmental positions and encouraging them to work outside the home and stay in school even if pregnant[http://newsreel.org/nav/title.asp?tc=CN0205].", "Burkina Faso"],
+    ["Communist countries are anti-LGBT/civil rights",
+    "LGBT, gay, rights, civil, homosexual",
+    "<li>While it is unfortunately true that countries such as the USSR had mixed policy on homosexuality, other movements for their time were progressive in decriminalising such acts, and to this day most Communist movements are pro-LGBT.</li><li>Russia in the USSR was <b>the first</b> major world power to grant women suffrage. [https://books.google.com/books?id=Qk-ICsx5L90C]</li><li>Burkina Faso under the leadership of Marxist revolutionary Thomas Sankara banned female genital mutilation, forced marriages and polygamy, while appointing women to high governmental positions and encouraging them to work outside the home and stay in school even if pregnant. [https://web.archive.org/web/20170930034205/http://newsreel.org/nav/title.asp?tc=CN0205]</li>",
+    "Long, sourced counter-arguments"],
 
-    ["Antifa are fascist", "fascism, fascist, antifa", "Short theoretical rebuttal", "Long, sourced counter-argument"]
+    ["Antifa are fascist",
+    "fascism, fascist, antifa",
+    "Short rebuttals",
+    "Long, sourced counter-arguments"]
 
 ];
 
 /* SOURCES FOR RESEARCH:
 https://www.reddit.com/r/communism/comments/6uhar8/comrades_reading_the_declassified_post_ww2/
 https://www.rbth.com/arts/2014/10/01/african-americans_search_for_equality_led_to_the_soviet_union_40113.html
+*/
+
+/* RULES AND GUIDE:
+- leave the second option (Long, sourced counter arguments) blank if there are none
+
 */
