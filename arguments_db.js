cea06e6490c6c6e462bbd58b518e97b30fb1b416
phunanon <phunanon@gmail.com>
2017-11-18 03:37:38 +0000

    Contribution: calories; added empty ref

--- arguments_db.js
+++ arguments_db.js
@@ -11,8 +11,8 @@ var arguments_DB = [
     "Long, sourced counter-arguments"],
 
     ["Communism is against human nature",
-    "human, nature, unnatural, greed, greedy",
-    "Short rebuttals",
+    "human, nature, unnatural, greedy, charity",
+    "<li>If arguing greed: Anti-capitalism is the embodiment of each-to-their-own: instead of wealth concentrating to the top <1% (e.g. 1 greedy person per 100) wealth is kept individually and shared at an individual's leisure (99 greedy people).</li><li>If arguing 'forced' egalitarianism: Egalitarianism is not inherent and guarenteed by socialism - it is the will of the working class. However, humans are naturally egalitarian (charitable), as seen through governmental programmes of helping those in need (welfare, disability benefits, etc), private charity, daily social engagements, and so forth.</li>",
     "Long, sourced counter-arguments"],
 
     ["Markets result in perfect allocation of resources",
@@ -52,27 +52,34 @@ var arguments_DB = [
 
     ["Socialist countries are always poor and hungry",
     "poverty, poor, hungry, hunger, famine, food, bread",
-    "<li>Within four years (1983-1987) Burkina Faso reached food self-sufficiency, producing double the regional average for wheat production. Before this, the old state ('Upper Volta') was a food-importing French neo-colonial capitalist state.[https://web.archive.org/web/20170929035718/http://africanagenda.net/africas-che-guevara-and-burkina-faso/]</li>",
+    "<li>Within four years (1983-1987) Burkina Faso reached food self-sufficiency, producing double the regional average for wheat production. Before this, the old state ('Upper Volta') was a food-importing French neo-colonial capitalist state.[https://web.archive.org/web/20170929035718/http://africanagenda.net/africas-che-guevara-and-burkina-faso/]</li><li>While people argue that countries such as the USA ate "better" than the USSR, this cannot be proven or disproven directly. Post-1960 data shows there was only a drop in calorie consumption as the USSR collapsed. The two nations had markedly different diets, but overall data does not represent this as it is unimportant - malnutrition would be very improbable on such a high calorie diet. [https://nintil.com/2016/05/11/the-soviet-union-food/]{Levi}</li>",
     "Long, sourced counter-arguments"],
 
     ["Communist countries are anti-LGBT/civil rights",
-    "LGBT, gay, rights, civil, homosexual",
+    "LGBT, gay, rights, civil, homosexualality",
     "<li>While it is unfortunately true that countries such as the USSR had mixed policy on homosexuality, other movements for their time were progressive in decriminalising such acts, and to this day most Communist movements are pro-LGBT.</li><li>Russia in the USSR was <b>the first</b> major world power to grant women suffrage. [https://books.google.com/books?id=Qk-ICsx5L90C]</li><li>Burkina Faso under the leadership of Marxist revolutionary Thomas Sankara banned female genital mutilation, forced marriages and polygamy, while appointing women to high governmental positions and encouraging them to work outside the home and stay in school even if pregnant. [https://web.archive.org/web/20170930034205/http://newsreel.org/nav/title.asp?tc=CN0205]</li>",
     "Long, sourced counter-arguments"],
 
     ["Antifa are fascist",
     "fascism, fascist, antifa",
     "Short rebuttals",
-    "Long, sourced counter-arguments"]
+    "Long, sourced counter-arguments"],
+
+    ["The majority of people were happy the USSR dissolved",
+    "USSR, happier, survey, dissolve"
+    "<li>[https://medium.com/@DavideMastracci/former-soviet-citizens-support-the-ussr-afdcb10c2225]</li><li>[http://www.pewresearch.org/fact-tank/2010/04/28/hungary-better-off-under-communism/]</li>"
+    ""]
 
 ];
 
 /* SOURCES FOR RESEARCH:
+https://www.reddit.com/r/communism101/comments/2pl8tv/rcommunism101s_frequently_asked_questions/
 https://www.reddit.com/r/communism/comments/6uhar8/comrades_reading_the_declassified_post_ww2/
 https://www.rbth.com/arts/2014/10/01/african-americans_search_for_equality_led_to_the_soviet_union_40113.html
 */
 
 /* RULES AND GUIDE:
 - leave the second option (Long, sourced counter arguments) blank if there are none
+- make tags as long as possible to aid searching (greed -> greedy)
 
 */
